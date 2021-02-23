d6e90d03afda7d038d90892ed56e31596facd5cd
KJH <demidemi1125@outlook.com>
2021-02-23 22:53:40 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,276 @@
+import os
+import sys
+
+# Flask
+from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+from io import BytesIO
+from absl import flags
+
+import src.config
+import tarfile
+import tempfile
+from six.moves import urllib
+
+
+import numpy as np
+from PIL import Image
+import cv2, pdb, glob, argparse
+from demo import main
+import tensorflow as tf
+#from inference import DeepLabModel
+
+'''
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+'''
+
+class DeepLabModel(object):
+	"""Class to load deeplab model and run inference."""
+
+	INPUT_TENSOR_NAME = 'ImageTensor:0'
+	OUTPUT_TENSOR_NAME = 'SemanticPredictions:0'
+	INPUT_SIZE = 513
+	FROZEN_GRAPH_NAME = 'frozen_inference_graph'
+
+	def __init__(self, tarball_path):
+		#"""Creates and loads pretrained deeplab model."""
+		self.graph = tf.Graph()
+		graph_def = None
+		# Extract frozen graph from tar archive.
+		tar_file = tarfile.open(tarball_path)
+		for tar_info in tar_file.getmembers():
+			if self.FROZEN_GRAPH_NAME in os.path.basename(tar_info.name):
+				file_handle = tar_file.extractfile(tar_info)
+				graph_def = tf.GraphDef.FromString(file_handle.read())
+				break
+
+		tar_file.close()
+
+		if graph_def is None:
+			raise RuntimeError('Cannot find inference graph in tar archive.')
+
+		with self.graph.as_default():
+			tf.import_graph_def(graph_def, name='')
+
+		self.sess = tf.Session(graph=self.graph)
+
+	def run(self, image):
+		"""Runs inference on a single image.
+
+		Args:
+		  image: A PIL.Image object, raw input image.
+
+		Returns:
+		  resized_image: RGB image resized from original input image.
+		  seg_map: Segmentation map of `resized_image`.
+		"""
+		width, height = image.size
+		resize_ratio = 1.0 * self.INPUT_SIZE / max(width, height)
+		target_size = (int(resize_ratio * width), int(resize_ratio * height))
+		resized_image = image.convert('RGB').resize(target_size, Image.ANTIALIAS)
+		batch_seg_map = self.sess.run(
+			self.OUTPUT_TENSOR_NAME,
+			feed_dict={self.INPUT_TENSOR_NAME: [np.asarray(resized_image)]})
+		seg_map = batch_seg_map[0]
+		return resized_image, seg_map
+
+def create_pascal_label_colormap():
+	"""Creates a label colormap used in PASCAL VOC segmentation benchmark.
+
+	Returns:
+	A Colormap for visualizing segmentation results.
+	"""
+	colormap = np.zeros((256, 3), dtype=int)
+	ind = np.arange(256, dtype=int)
+
+	for shift in reversed(range(8)):
+		for channel in range(3):
+		  colormap[:, channel] |= ((ind >> channel) & 1) << shift
+		ind >>= 3
+
+	return colormap
+
+def label_to_color_image(label):
+	"""Adds color defined by the dataset colormap to the label.
+
+	Args:
+	label: A 2D array with integer type, storing the segmentation label.
+
+	Returns:
+	result: A 2D array with floating type. The element of the array
+	  is the color indexed by the corresponding element in the input label
+	  to the PASCAL color map.
+
+	Raises:
+	ValueError: If label is not of rank 2 or its value is larger than color
+	  map maximum entry.
+	"""
+	if label.ndim != 2:
+		raise ValueError('Expect 2-D input label')
+
+	colormap = create_pascal_label_colormap()
+
+	if np.max(label) >= len(colormap):
+		raise ValueError('label value too large.')
+
+	return colormap[label]
+
+
+# Some utilites
+import numpy as np
+from util import base64_to_pil
+
+
+
+
+
+# Declare a flask app
+app = Flask(__name__)
+
+# You can use pretrained model from Keras
+# Check https://keras.io/applications/
+# or https://www.tensorflow.org/api_docs/python/tf/keras/applications
+
+'''
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+model = MobileNetV2(weights='imagenet')
+'''
+
+LABEL_NAMES = np.asarray([
+	'background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
+	'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
+	'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tv'
+])
+
+FULL_LABEL_MAP = np.arange(len(LABEL_NAMES)).reshape(len(LABEL_NAMES), 1)
+FULL_COLOR_MAP = label_to_color_image(FULL_LABEL_MAP)
+
+
+MODEL_NAME = 'xception_coco_voctrainval'  # @param ['mobilenetv2_coco_voctrainaug', 'mobilenetv2_coco_voctrainval', 'xception_coco_voctrainaug', 'xception_coco_voctrainval']
+
+_DOWNLOAD_URL_PREFIX = 'http://download.tensorflow.org/models/'
+_MODEL_URLS = {
+	'mobilenetv2_coco_voctrainaug':
+		'deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz',
+	'mobilenetv2_coco_voctrainval':
+		'deeplabv3_mnv2_pascal_trainval_2018_01_29.tar.gz',
+	'xception_coco_voctrainaug':
+		'deeplabv3_pascal_train_aug_2018_01_04.tar.gz',
+	'xception_coco_voctrainval':
+		'deeplabv3_pascal_trainval_2018_01_04.tar.gz',
+}
+_TARBALL_NAME = _MODEL_URLS[MODEL_NAME]
+
+model_dir = '../HBMC_models/deeplab_model'
+if not os.path.exists(model_dir):
+  tf.gfile.MakeDirs(model_dir)
+
+download_path = os.path.join(model_dir, _TARBALL_NAME)
+if not os.path.exists(download_path):
+  print('downloading model to %s, this might take a while...' % download_path)
+  urllib.request.urlretrieve(_DOWNLOAD_URL_PREFIX + _MODEL_URLS[MODEL_NAME], 
+			     download_path)
+  print('download completed! loading DeepLab model...')
+
+MODEL = DeepLabModel(download_path)
+print('model loaded successfully!')
+
+#######################################################################################
+
+
+
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+
+
+# Model saved with Keras model.save()
+#MODEL_PATH = 'models/your_model.h5'
+
+# Load your own trained model
+# model = load_model(MODEL_PATH)
+# model._make_predict_function()          # Necessary
+print('Model loaded. Start serving...')
+
+
+
+'''
+def model_predict(img, model):
+    img = img.resize((224, 224))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    x = preprocess_input(x, mode='tf')
+
+    preds = model.predict(x)
+    return preds
+'''
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        # Get the image from post request
+        image = base64_to_pil(request.json)
+        res_im,seg=MODEL.run(image)
+
+        seg=cv2.resize(seg.astype(np.uint8),image.size)
+        mask_sel=(seg==15).astype(np.float32)
+        mask = 255*mask_sel.astype(np.uint8)
+
+        img = 	np.array(image)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)   
+
+        res = cv2.bitwise_and(img,img,mask = mask)
+        bg_removed = res + (255 - cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)) 
+        result = main(bg_removed,180,None)
+        # Save the image to ./uploads
+        # img.save("./uploads/image.png")
+
+        # Make prediction
+        #preds = model_predict(img, model)
+
+        # Process your result for human
+        #pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
+        #pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+
+        #result = str(pred_class[0][0][1])               # Convert to string
+
+        #result = "Human"
+        #result = result.replace('_', ' ').capitalize()
+        
+        # Serialize the result, you can add additional fields
+        #return jsonify(result=result, probability=pred_proba)
+        return jsonify(result=result)
+
+    return None
+
+
+if __name__ == '__main__':
+    # app.run(port=5002, threaded=False)
+    print("main start")
+    # Serve the app with gevent
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    print("main end")
+    http_server.serve_forever()
+    print("main end1")
+
+
